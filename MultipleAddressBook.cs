c5d620e308c837ddca3320362259452492efbb52
Satantra Tewari <satantra1508@gmail.com>
2020-10-19 17:10:56 +0530

    [satantra] code refactored to add and display multiple address books

---
+++ MultipleAddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Dynamic;
+using System.Linq;
+
+namespace Address_Book_Problem
+{
+    public class MultipleAddressBook
+    {
+        Dictionary<string, AddressBookMain> multipleAddressBooks = new Dictionary<string, AddressBookMain>();
+        AddressBookMain addressBookMain = new AddressBookMain();
+        public MultipleAddressBook()
+        {
+            multipleAddressBooks = new Dictionary<string, AddressBookMain>();
+        }
+        public void AddNewAddressBook(string nameOfAddressBook)
+        {
+            multipleAddressBooks.Add(nameOfAddressBook, addressBookMain);
+        }
+        public AddressBookMain GetAddressBook(string nameOfAddressBook)
+        {
+            if (multipleAddressBooks.ContainsKey(nameOfAddressBook))
+                return multipleAddressBooks[nameOfAddressBook];
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,35 +6,70 @@ namespace Address_Book_Problem
     {
         static void Main(string[] args)
         {
+            string nameOfAddressBook = "";
+            int choiceOfOperation;
+            int choiceOfAddressBookOperation;
             Console.WriteLine(" Welcome to Address Book Program ");
             Console.WriteLine(" =============================== ");
-            AddressBookMain addressBookMain = new AddressBookMain();
-            Console.WriteLine("Please enter your choice of operation(1 to 4)");
-            Console.WriteLine("\n1.ADD CONTACT\n2.EDIT CONTACT USING FIRST NAME\n3.REMOVE CONTACT USING FIRST NAME\n4.DISPLAY BOOK\n");
-            int choiceOfOperation = Convert.ToInt32(Console.ReadLine());
-        OperationsOnAddressBook:
-            switch (choiceOfOperation)
+            AddressBookMain addressBookMain = null;
+            MultipleAddressBook multipleAddressBook = new MultipleAddressBook();
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Enter number of contacts to be added");
-                    int numberOfContacts = Convert.ToInt32(Console.ReadLine());
-                    for(int i=1;i<=numberOfContacts;i++)
+                bool flag = true;
+                Console.WriteLine("Enter your choice of operation(1 or 2)\n1.ADD ADDRESS BOOK\n2.GET ADDRESS BOOK\n");
+                choiceOfOperation = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter name of Address Book");
+                nameOfAddressBook = Console.ReadLine();
+                switch (choiceOfOperation)
+                {
+                    case 1:
+                        multipleAddressBook.AddNewAddressBook(nameOfAddressBook);
+                        addressBookMain = multipleAddressBook.GetAddressBook(nameOfAddressBook);
+                        break;
+                    case 2:
+                        addressBookMain = multipleAddressBook.GetAddressBook(nameOfAddressBook);
+                        if (addressBookMain == null)
+                            Console.WriteLine("No such address book found");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+                while (flag)
+                {
+                    if (addressBookMain == null)
+                        break;
+                    OperationsOnAddressBook:
+                    Console.WriteLine("Please enter your choice of operation(1 to 4)");
+                    Console.WriteLine("\n1.ADD CONTACT\n2.EDIT CONTACT USING FIRST NAME\n3.REMOVE CONTACT USING FIRST NAME\n4.DISPLAY BOOK\n5.EXIT");
+                    choiceOfAddressBookOperation = Convert.ToInt32(Console.ReadLine());
+                    switch (choiceOfAddressBookOperation)
                     {
-                        addressBookMain.AddContactToBook();
+                        case 1:
+                            Console.WriteLine("Enter number of contacts you want to add");
+                            int numberOfContacts = Convert.ToInt32(Console.ReadLine());
+                            for (int i = 1; i <= numberOfContacts; i++)
+                            {
+                                addressBookMain.AddContactToBook();
+                            }
+                            break;
+                        case 2:
+                            addressBookMain.EditUsingFirstName();
+                            break;
+                        case 3:
+                            addressBookMain.RemoveUsingFirstName();
+                            break;
+                        case 4:
+                            addressBookMain.DisplayAddressBook();
+                            break;
+                        case 5:
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter valid choice");
+                            goto OperationsOnAddressBook;
                     }
-                    break;
-                case 2:
-                    addressBookMain.EditUsingFirstName();
-                    break;
-                case 3:
-                    addressBookMain.RemoveUsingFirstName();
-                    break;
-                case 4:
-                    addressBookMain.DisplayAddressBook();
-                    break;
-                default:
-                    Console.WriteLine("Please enter correct choice(1 to 4)");
-                    goto OperationsOnAddressBook;
+                }
             }
         }
     }
